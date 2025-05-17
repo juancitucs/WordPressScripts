dbf5b4417d9cd24c5abb06fbf746b13a263d59cc
juancitucs <juancitolinuxlover@gmail.com>
2025-05-17 17:40:34 -0500

    duplicated config and 50-static.yml removed

--- god.sh
+++ god.sh
@@ -69,42 +69,7 @@ network:
         - to: 0.0.0.0/0
           via: 20.20.20.1
           metric: 200
-EOF
-network:
-  version: 2
-  ethernets:
-    enp0s3:
-      dhcp4: no
-      addresses:
-        - ${IP3}/${NETMASK}
-      nameservers:
-        addresses: [8.8.8.8, 8.8.4.4]
-    enp0s8:
-      dhcp4: no
-      addresses:
-        - ${IP8}/${NETMASK}
-      nameservers:
-        addresses: [8.8.8.8, 8.8.4.4]
-      routes:
-        - to: 0.0.0.0/0
-          via: 20.20.20.1
-          metric: 100
-EOF
-network:
-  version: 2
-  ethernets:
-    enp0s3:
-      dhcp4: no
-      addresses:
-        - ${IP3}/${NETMASK}
-      nameservers:
-        addresses: [8.8.8.8, 8.8.4.4]
-    enp0s8:
-      dhcp4: no
-      addresses:
-        - ${IP8}/${NETMASK}
-      nameservers:
-        addresses: [8.8.8.8, 8.8.4.4]
+       addresses: [8.8.8.8, 8.8.4.4]
 EOF
   netplan apply
   echo "IPs: enp0s3=$IP3, enp0s8=$IP8"
